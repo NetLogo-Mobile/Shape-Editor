f1cfe0cb93a73c06acba58aeb8631722b2f26718
wubbalubbadu <wavgoddbye@gmail.com>
2023-04-15 13:14:10 -0500

    added root container and css linting, some styling

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,29 @@
 module.exports = {
-  extends: ["eslint:recommended", "plugin:prettier/recommended"],
-  plugins: ["svelte3"],
-    parserOptions: {
-      ecmaVersion: 2020,
-      sourceType: "module",
+  extends: [
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+    "plugin:css/recommended"
+  ],
+  plugins: ["svelte3", "css"],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  env: {
+    browser: true,
+    es6: true,
+  },
+  overrides: [
+    {
+      files: ["*.svelte"],
+      processor: "svelte3/svelte3",
     },
-    env: {
-      browser: true,
-      es6: true,
-    },
-    rules: {},
-    settings: {
-      "svelte3/ignore-styles": (attrs) => attrs.lang === "scss",
-    },
-  };
-  
+  ],
+  settings: {
+    "svelte3/ignore-styles": (attrs) => attrs.lang === "scss",
+  },
+  rules: {
+    "css/no-duplicate-selectors": ["error", { "count": 1 }],
+    // Add more CSS rules here
+  }
+};
--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,21 @@
 <script>
-
   let shapes = [
-    { id: 1, name: 'Circle' },
-    { id: 2, name: 'Square' },
-    { id: 3, name: 'Triangle' },
-    { id: 4, name: 'Rectangle' },
-    { id: 5, name: 'Star' },
-    { id: 6, name: 'Heart' },
-    { id: 7, name: 'Diamond' },
-    { id: 8, name: 'Pentagon' },
-    { id: 9, name: 'Hexagon' },
-    { id: 10, name: 'Octagon' },
-    { id: 11, name: 'Circle' },
-    { id: 12, name: 'Square' },
-    { id: 13, name: 'Triangle' },
-    { id: 14, name: 'Rectangle' },
-    { id: 15, name: 'Star' },
-    { id: 16, name: 'Heart' },
-    { id: 17, name: 'Diamond' },
-    { id: 18, name: 'Pentagon' },
-    { id: 19, name: 'Hexagon' },
-    { id: 20, name: 'Octagon' },
+    { id: 1, name: 'Circle', image: 'circle' },
+    { id: 2, name: 'Square', image: 'square' },
+    { id: 3, name: 'Circle', image: 'circle' },
+    { id: 4, name: 'Square', image: 'square' },
+    { id: 5, name: 'Circle', image: 'circle' },
+    { id: 6, name: 'Square', image: 'square' },
+    { id: 7, name: 'Circle', image: 'circle' },
+    { id: 8, name: 'Square', image: 'square' },
+    { id: 9, name: 'Circle', image: 'circle' },
+    { id: 10, name: 'Square', image: 'square' },
+    { id: 11, name: 'Circle', image: 'circle' },
+    { id: 12, name: 'Square', image: 'square' },
+    { id: 13, name: 'Circle', image: 'circle' },
+    { id: 14, name: 'Square', image: 'square' },
+    { id: 15, name: 'Circle', image: 'circle' },
+    { id: 16, name: 'Square', image: 'square' },
   ];
 
   let searchTerm = '';
@@ -34,118 +29,49 @@
   }
 
   function duplicateShape(id) {
+    console.log(id);
     // Code to duplicate a shape with given id
   }
 
   function deleteShape(id) {
+    console.log(id);
     // Code to delete a shape with given id
   }
 </script>
 
-<div class="shape-selector">
-  <div class="shape-selector-header">
-    <img src='header-logo.png' alt="header logo"/>
-    <h2>Shape Editor</h2>
-  </div>
-  <div class="inner-container">
-    <div class="mode-selector">
-      <h3>Selection Mode</h3>
-      <div class="selector-buttons">
-        <div class="mode-selector-buttons">
-          <button class="turtle-button" on:click={() => console.log('turtle')}><img src="turtle.png" alt="turtle button"/>Turtle</button>
-          <button class="link-button" on:click={() => console.log('link')}><img src="link.png" alt="link button"/>Link</button>
-        </div>
-        <div class="shape-selector-buttons">
-          <button class="create-new-button" on:click={createShape}>Create New</button>
-          <button class="import-shapes-button" on:click={importShapes}>Import From...</button>
-        </div>
-      </div>
-    </div>
-    <div class="shape-selector-search">
-      <input bind:value={searchTerm} placeholder="Search" />
-    </div>
-    <div class="shape-selector-grid">
-      <div class="shape-selector-grid-inner">
-        {#each shapes.filter((shape) => shape.name
-            .toLowerCase()
-            .includes(searchTerm.toLowerCase())) as shape}
-          <div
-            class="shape-selector-item"
-            on:mouseover={() => (shape.hover = true)}
-            on:mouseout={() => (shape.hover = false)}
-            on:focus={() => (shape.hover = true)}
-            on:blur={() => (shape.hover = false)}
-          >
-            {#if shape.hover}
-              <div class="shape-selector-item-buttons">
-                <span
-                  on:click={() => deleteShape(shape.id)}
-                  on:keydown={(event) => {
-                    if (event.key === 'Enter') deleteShape(shape.id);
-                  }}
-                  role="button"
-                  tabindex="0"
-                  aria-label="Delete shape"
-                  class="material-icons"
-                >
-                  delete
-                </span>
-                <span
-                  on:click={() => duplicateShape(shape.id)}
-                  on:keydown={(event) => {
-                    if (event.key === 'Enter') duplicateShape(shape.id);
-                  }}
-                  role="button"
-                  tabindex="0"
-                  aria-label="Duplicate shape"
-                  class="material-icons"
-                >
-                  file_copy
-                </span>
-              </div>
-            {/if}
-            <div class="shape-selector-item-name">{shape.name}</div>
-          </div>
-        {/each}
-      </div>
-    </div>
-
-  </div>
-</div>
-
 <style>
-  .shape-selector {
+  .shape-selector-dialog .shape-selector {
     box-sizing: border-box;
     position: absolute;
     width: 500px;
     height: 362px;
     left: 200px;
     top: 113.5px;
-    background: #EEEFF0;
-    border: 1px solid #C0C0C0;
+    background: #eeeff0;
+    border: 1px solid #c0c0c0;
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
     border-radius: 5px;
     /* display: flex;
     flex-direction: column; */
   }
 
-  .inner-container{
+  .shape-selector-dialog .inner-container {
     display: flex;
     flex-direction: column;
     padding: 0 25px 30px 25px;
   }
-  .shape-selector-header {
+  .shape-selector-dialog .shape-selector-header {
     display: flex;
     align-items: center;
     margin-bottom: 10px;
     width: 100%;
     height: 36px;
-    background: #5A648D;
+    background: #5a648d;
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
     border-radius: 5px 5px 0px 0px;
   }
 
-  .shape-selector-header h2{
+  .shape-selector-dialog .shape-selector-header h2 {
     font-family: 'Lato';
     font-style: normal;
     font-weight: 800;
@@ -153,33 +79,33 @@
     line-height: 14px;
     display: flex;
     align-items: center;
-    color: #FFFFFF;
+    color: #ffffff;
     text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
     margin: 0;
   }
-  .selector-buttons{
+  .shape-selector-dialog .selector-buttons {
     display: flex;
     justify-content: space-between;
     width: 100%;
     margin-top: 2px;
   }
-  .shape-selector-header img {
+  .shape-selector-dialog .shape-selector-header img {
     width: 24px;
     height: 24px;
     margin: 7px 10px 0px 10px;
   }
 
-  .mode-selector {
+  .shape-selector-dialog .mode-selector {
     display: flex;
     flex-direction: column;
     align-items: flex-start;
   }
-  .turtle-button {
+  .shape-selector-dialog .turtle-button {
     width: 50px;
     height: 20px;
     box-sizing: border-box;
-    background: #5A648D;
-    border: 0.3px solid #CECECE;
+    background: #5a648d;
+    border: 0.3px solid #cecece;
     box-shadow: inset 0px 4px 4px rgba(0, 0, 0, 0.25);
     border-radius: 2px;
     font-family: 'Lato';
@@ -189,22 +115,22 @@
     line-height: 11px;
     display: flex;
     align-items: center;
-    color: #FFFFFF;
+    color: #ffffff;
     padding-left: 0px;
   }
 
-  .turtle-button img {
+  .shape-selector-dialog .turtle-button img {
     width: 20px;
     height: 20px;
     margin: 13px 0px 5px 0px;
   }
 
-  .link-button{
+  .shape-selector-dialog .link-button {
     box-sizing: border-box;
     width: 44px;
     height: 20px;
-    background: #E5E5E5;
-    border: 0.3px solid #CECECE;
+    background: #e5e5e5;
+    border: 0.3px solid #cecece;
     border-radius: 2px;
     font-family: 'Lato';
     font-style: normal;
@@ -215,13 +141,13 @@
     align-items: center;
     color: #000000;
   }
-  .link-button img {
+  .shape-selector-dialog .link-button img {
     width: 11px;
     height: 11px;
     margin: 6px 4px 5px 0px;
   }
 
-  .mode-selector h3{
+  .shape-selector-dialog .mode-selector h3 {
     font-family: 'Lato';
     font-style: normal;
     font-weight: 700;
@@ -233,25 +159,25 @@
     margin: 0;
   }
 
-  .mode-selector-buttons {
+  .shape-selector-dialog .mode-selector-buttons {
     display: flex;
     margin-left: 0px;
     flex-wrap: wrap;
     gap: 10px;
   }
 
-.shape-selector-buttons {
-  display: flex;
-  justify-content: space-between;
-}
+  .shape-selector-dialog .shape-selector-buttons {
+    display: flex;
+    justify-content: space-between;
+  }
 
-  .create-new-button {
+  .shape-selector-dialog .create-new-button {
     box-sizing: border-box;
     width: 90px;
     height: 20px;
     box-sizing: border-box;
-    background: #BACFF3;
-    border: 0.3px solid #CECECE;
+    background: #bacff3;
+    border: 0.3px solid #cecece;
     border-radius: 2px;
     font-family: 'Lato';
     font-style: normal;
@@ -260,18 +186,16 @@
     line-height: 11px;
     display: flex;
     align-items: center;
-
-    color: #FFFFFF;
-
+    color: #ffffff;
     text-shadow: 0px 2px 3px rgba(0, 0, 0, 0.25);
   }
-  
-  .import-shapes-button {
+
+  .shape-selector-dialog .import-shapes-button {
     width: 98px;
     height: 20px;
     box-sizing: border-box;
-    background: #D9D9D9;
-    border: 0.3px solid #CECECE;
+    background: #d9d9d9;
+    border: 0.3px solid #cecece;
     border-radius: 2px;
 
     font-family: 'Lato';
@@ -282,12 +206,11 @@
     display: flex;
     align-items: center;
 
-    color: #FFFFFF;
-
-  text-shadow: 0px 2px 3px rgba(0, 0, 0, 0.25);
+    color: #ffffff;
+    text-shadow: 0px 2px 3px rgba(0, 0, 0, 0.25);
   }
 
-  .shape-selector-buttons button {
+  .shape-selector-dialog .shape-selector-buttons button {
     margin-left: 10px;
   }
 
@@ -295,11 +218,11 @@
     box-sizing: border-box;
     width: 450px;
     height: 28px;
-    background: #FFFFFF;
-    border: 1px solid #CECECE;
+    background: #ffffff;
+    border: 1px solid #cecece;
     border-radius: 5px;
   }
-  .shape-selector-search input {
+  .shape-selector-dialog .shape-selector-search input {
     width: 100%;
     height: 100%;
     border: none;
@@ -309,42 +232,152 @@
     font-style: normal;
     font-weight: 400;
     font-size: 9px;
-line-height: 11px;
+    line-height: 11px;
     display: flex;
     align-items: center;
-    color: #9E9E9E;
+    color: #9e9e9e;
   }
 
-  .shape-selector-grid {
-  flex: 1;
-  overflow: hidden;
-  background: #FFFFFF;
-  box-shadow: inset 2px 3px 4px rgba(0, 0, 0, 0.25);
-  border-radius: 5px 2px 2px 5px;
-  margin-top: 14px;
-  padding: 14px 14px 0px 14px;
-}
-  .shape-selector-grid-inner {
-  overflow-y: auto;
-  display: grid;
-  grid-template-columns: repeat(5, 1fr);
-  grid-gap: 5px;
-  width: 100%;
-  height: 180px;
-  box-sizing: border-box;
-}
+  .shape-selector-dialog .shape-selector-grid {
+    flex: 1;
+    overflow: hidden;
+    background: #ffffff;
+    box-shadow: inset 2px 3px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 5px 2px 2px 5px;
+    margin-top: 14px;
+    padding: 14px 14px 0px 14px;
+  }
+  .shape-selector-dialog .shape-selector-grid-inner {
+    overflow-y: auto;
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-gap: 5px;
+    width: 100%;
+    height: 180px;
+    box-sizing: border-box;
+  }
 
-  .shape-selector-item {
+  .shape-selector-dialog .shape-selector-item {
     position: relative;
     display: flex;
     justify-content: center;
     align-items: center;
     background-color: #f2f2f2;
 
-    width: 75px;
-    height: 68px;
-    background: #FFFFFF;
-    border: 0.7px solid #CECECE;
+    width: 79px;
+    height: 71px;
+    margin: 0px;
+    background: #ffffff;
+    border: 0.7px solid #cecece;
     border-radius: 3px;
   }
+
+  .shape-selector-dialog .shape-selector-details {
+    padding: 5px, 20px, 5px, 20px;
+  }
+
+  .shape-selector-dialog .shape-selector-item-buttons {
+    position: absolute;
+    top: 3px;
+    right: 3px;
+    display: flex;
+    flex-direction: row;
+  }
+
+  .shape-selector-dialog .shape-selector-item-buttons span {
+    width: 16px;
+    height: 16px;
+    box-sizing: border-box;
+    background: #ffffff;
+    border: 0.7px solid #cecece;
+    border-radius: 2px;
+    font-size: 10px;
+    padding: 2.5px;
+    margin-right: 1px;
+  }
 </style>
+
+<div class="shape-selector-dialog">
+  <div class="shape-selector">
+    <div class="shape-selector-header">
+      <img src="header-logo.png" alt="header logo" />
+      <h2>Shape Editor</h2>
+    </div>
+    <div class="inner-container">
+      <div class="mode-selector">
+        <h3>Selection Mode</h3>
+        <div class="selector-buttons">
+          <div class="mode-selector-buttons">
+            <button class="turtle-button" on:click={() => console.log('turtle')}
+              ><img src="turtle.png" alt="turtle button" />Turtle</button
+            >
+            <button class="link-button" on:click={() => console.log('link')}
+              ><img src="link.png" alt="link button" />Link</button
+            >
+          </div>
+          <div class="shape-selector-buttons">
+            <button class="create-new-button" on:click={createShape}
+              ><span class="material-icons" />Create New</button
+            >
+            <button class="import-shapes-button" on:click={importShapes}
+              ><span class="material-icons" />Import From...</button
+            >
+          </div>
+        </div>
+      </div>
+      <div class="shape-selector-search">
+        <input bind:value={searchTerm} placeholder="Search" />
+      </div>
+      <div class="shape-selector-grid">
+        <div class="shape-selector-grid-inner">
+          {#each shapes.filter((shape) => shape.name
+              .toLowerCase()
+              .includes(searchTerm.toLowerCase())) as shape}
+            <div
+              class="shape-selector-item"
+              on:mouseover={() => (shape.hover = true)}
+              on:mouseout={() => (shape.hover = false)}
+              on:focus={() => (shape.hover = true)}
+              on:blur={() => (shape.hover = false)}
+            >
+              {#if shape.hover}
+                <div class="shape-selector-item-buttons">
+                  <span
+                    on:click={() => duplicateShape(shape.id)}
+                    on:keydown={(event) => {
+                      if (event.key === 'Enter') duplicateShape(shape.id);
+                    }}
+                    role="button"
+                    tabindex="0"
+                    aria-label="Duplicate shape"
+                    class="material-icons"
+                  >
+                    file_copy
+                  </span>
+                  <span
+                    on:click={() => deleteShape(shape.id)}
+                    on:keydown={(event) => {
+                      if (event.key === 'Enter') deleteShape(shape.id);
+                    }}
+                    role="button"
+                    tabindex="0"
+                    aria-label="Delete shape"
+                    class="material-icons"
+                  >
+                    delete
+                  </span>
+                </div>
+              {/if}
+              <div class="shape-selector-details">
+                <div class="shape-selector-item-name">{shape.name}</div>
+                <div>
+                  <span class="material-ui">pentagon</span>
+                </div>
+              </div>
+            </div>
+          {/each}
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
