319e469f5514ef91f735cca605206bce6441bd2b
eniraa <47072520+eniraa@users.noreply.github.com>
2023-05-31 18:33:39 -0400

    fix type issues

--- src/editor/Editor.svelte
+++ src/editor/Editor.svelte
@@ -1,59 +1,65 @@
 <script lang="ts">
-    import ToolButton from "./ToolButton.svelte";
-    import { Tool } from "./tool";
-    import Canvas from "./Canvas.svelte";
+  import ToolButton from './ToolButton.svelte';
+  import { Tool } from './tool';
+  import Canvas from './Canvas.svelte';
+  import { JSONShape } from './json_shape';
 
-    let name: string = "default";
-    let current_tool: Tool = Tool.Select;
-    let current_color: Color = "#FFFFFF";
-    let editable_color_index: number = 0;
-    let rotate: boolean = true;
+  let name: string = 'default';
+  let current_tool: Tool = Tool.Select;
+  let current_color: Color = '#FFFFFF';
+  let editable_color_index: number = 0;
+  let rotate: boolean = true;
 
-    let canvasImport;
-    let canvasReset;
+  let canvasImport: (shape: JSONShape) => void;
+  let canvasReset: () => void;
 
-    export const open = () => {
-        const dialog: HTMLDialogElement = document.querySelector("#editor");
-        dialog.showModal();
-    };
+  export const open = () => {
+    const dialog: HTMLDialogElement = document.querySelector('#editor')!;
+    dialog.showModal();
+  };
 
-    export const openWithJSON = (input: string) => {
-        canvasImport(JSON.parse(input));
-        const dialog: HTMLDialogElement = document.querySelector("#editor");
-        dialog.showModal();
-    };
+  export const openWithJSON = (input: string) => {
+    canvasImport(JSON.parse(input));
+    const dialog: HTMLDialogElement = document.querySelector('#editor')!;
+    dialog.showModal();
+  };
 
-    const close = () => {
-        canvasReset();
-        const dialog: HTMLDialogElement = document.querySelector("#editor");
-        dialog.close();
-    };
+  const close = () => {
+    canvasReset();
+    const dialog: HTMLDialogElement = document.querySelector('#editor')!;
+    dialog.close();
+  };
 </script>
 
 <dialog id="editor">
-    <h1 contenteditable="true" bind:textContent={name}>{name}</h1>
-    <button on:click={close}>Close</button>
-    <ToolButton bind:current_tool button_tool={Tool.Select}>Select</ToolButton>
-    <ToolButton bind:current_tool button_tool={Tool.DrawRectangle}
-        >Rectangle</ToolButton
-    >
-    <ToolButton bind:current_tool button_tool={Tool.DrawCircle}
-        >Circle</ToolButton
-    >
-    <ToolButton bind:current_tool button_tool={Tool.DrawPolygon}
-        >Polygon</ToolButton
-    >
-    <ToolButton bind:current_tool button_tool={Tool.DrawLine}>Line</ToolButton>
-    <ToolButton bind:current_tool button_tool={Tool.DrawFilledRectangle}>Filled Rectangle</ToolButton>
-    <ToolButton bind:current_tool button_tool={Tool.DrawFilledCircle}>Filled Circle</ToolButton>
-    <ToolButton bind:current_tool button_tool={Tool.DrawFilledPolygon}>Filled Polygon</ToolButton>
-    <Canvas
-        bind:current_tool
-        bind:current_color
-        bind:editable_color_index
-        bind:name
-        bind:rotate
-        bind:importShape={canvasImport}
-        bind:reset={canvasReset}
-    />
+  <h1 contenteditable="true" bind:textContent={name}>{name}</h1>
+  <button on:click={close}>Close</button>
+  <ToolButton bind:current_tool button_tool={Tool.Select}>Select</ToolButton>
+  <ToolButton bind:current_tool button_tool={Tool.DrawRectangle}
+    >Rectangle</ToolButton
+  >
+  <ToolButton bind:current_tool button_tool={Tool.DrawCircle}>Circle</ToolButton
+  >
+  <ToolButton bind:current_tool button_tool={Tool.DrawPolygon}
+    >Polygon</ToolButton
+  >
+  <ToolButton bind:current_tool button_tool={Tool.DrawLine}>Line</ToolButton>
+  <ToolButton bind:current_tool button_tool={Tool.DrawFilledRectangle}
+    >Filled Rectangle</ToolButton
+  >
+  <ToolButton bind:current_tool button_tool={Tool.DrawFilledCircle}
+    >Filled Circle</ToolButton
+  >
+  <ToolButton bind:current_tool button_tool={Tool.DrawFilledPolygon}
+    >Filled Polygon</ToolButton
+  >
+  <Canvas
+    bind:current_tool
+    bind:current_color
+    bind:editable_color_index
+    bind:name
+    bind:rotate
+    bind:importShape={canvasImport}
+    bind:reset={canvasReset}
+  />
 </dialog>
--- src/editor/ToolButton.svelte
+++ src/editor/ToolButton.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
-    import { Tool } from "./tool";
+  import { Tool } from './tool';
 
-    export let current_tool: Tool = Tool.Select;
-    export let button_tool: Tool = Tool.Select;
+  export let current_tool: Tool = Tool.Select;
+  export let button_tool: Tool = Tool.Select;
 
-    const mutate_tool = () => {
-        console.log(current_tool);
-        current_tool = button_tool;
-        console.log(current_tool);
-    }
+  const mutate_tool = () => {
+    console.log(current_tool);
+    current_tool = button_tool;
+    console.log(current_tool);
+  };
 </script>
 
 <button on:click={mutate_tool}>
-    <slot></slot>
+  <slot />
 </button>
